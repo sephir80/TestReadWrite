bd4997de9a91f563a99fbc4734d764a3ac0d9eaf
Alessandro Tognon <sephir80@icloud.com>
2024-11-19 21:52:11 +0100

    Initial Commit

---
+++ TestReadWrite/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestReadWrite
+//
+//  Created by Alessandro Tognon on 19/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
